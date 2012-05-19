8f80edee7d07c3fe44974da32b3670f1658fb36e
Nick Meharry <nick@nickmeharry.com>
2012-05-18 22:24:05 -0700

    Add install script for new setups.

---
+++ install-dotfiles.zsh
@@ -0,0 +1,30 @@
+#!/usr/bin/env zsh
+
+abspath () {
+    case "$1" in
+        /*)
+        printf "%s\n" "$1"
+        ;;
+
+        *)
+        printf "%s\n" "$PWD/$1"
+        ;;
+    esac
+}
+
+dotfiles=(emacs.d gitconfig hgrc)
+
+dotfiles_dir="$(dirname $(abspath $0))"
+
+cd $HOME
+
+for df in $dotfiles; do
+    source="$dotfiles_dir/$df"
+    target=".$df"
+    if [[ -a $target ]]; then
+        print "\e[31mWarning! \"$target\" already exists!\e[0m"
+    else
+        print Linking $source to $target
+        ln -s $souce $target
+    fi
+done
