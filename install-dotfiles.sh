0c845103bbaa3ac72f0b0e2f1557fddc476af562
Nick Meharry <nick@nickmeharry.com>
2012-05-23 14:24:34 -0700

    Port install script to sh, as some setups don't have zsh installed.

---
+++ install-dotfiles.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+abspath () {
+    case "$1" in
+        /*)
+        printf "%s\n" "$1"
+        ;;
+
+        *)
+        printf "%s\n" "$PWD/$1"
+        ;;
+    esac
+}
+
+dotfiles="
+    emacs.d
+
+    gitconfig
+    hgrc
+
+    zshrc
+    zshenv
+    zprompt
+    zsh.d
+
+    ssh/config
+"
+
+dotfiles_dir="$(dirname $(abspath $0))"
+
+cd $HOME
+
+for df in $dotfiles; do
+    source="$dotfiles_dir/$df"
+    target=".$df"
+    if [ $target != "$(basename $target)" -a ! -e "$(dirname $target)" ]; then
+	echo "\033[33mCreating directories \"$(dirname $target)\"\033[0m"
+	mkdir -p "$(dirname $target)"
+    fi
+    if [ -e $target ]; then
+        echo "\033[31mWarning! \"$target\" already exists!\033[0m"
+    else
+        echo "Linking $source to $target"
+        ln -s $source $target
+    fi
+done
