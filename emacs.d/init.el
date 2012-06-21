199746c5d9fa88d3a8487311d78a79b39aca857a
Nick Meharry <nick@drchrono.com>
2012-06-20 20:27:33 -0700

    Emacs IRC settings.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,21 @@
+;; Helper Functions
+(defun chomp (str)
+  "Chomp leading and tailing whitespace from STR."
+  (while (string-match "\\`\n+\\|^\\s-+\\|\\s-+$\\|\n+\\'"
+                       str)
+    (setq str (replace-match "" t t str)))
+  str)
+
+(defun file-lines (file)
+  "Read a file and return lines as a list."
+  (with-temp-buffer
+    (insert-file-contents file)
+    (split-string (buffer-string) "\n")))
+
+(defun file-first-line (file)
+  "Read the first line from a file. Handy for passwords."
+  (chomp (car (file-lines file))))
+
 ;; Highlight selections (marks)
 (setq transient-mark-mode t)
 
@@ -41,3 +59,23 @@
   (interactive)
   "kill-all-buffers"
   (mapc 'kill-buffer (buffer-list)))
+
+;; Grove.io IRC
+; NickServ
+(add-hook 'erc-after-connect
+	  '(lambda (SERVER NICK)
+	     (erc-message "PRIVMSG"
+			  (concat "NickServ identify "
+				  (file-first-line "~/.emacs.d/grove-irc-password.txt")))))))
+; SSL
+(require 'tls)
+(setq tls-program
+      '("openssl s_client -connect %h:%p -no_ssl2 -ign_eof"
+        "gnutls-cli --priority secure256 -p %p %h"))
+; Grove
+(defun connect-to-grove ()
+  "Connect to Grove.io IRC"
+  (interactive)
+  (erc-tls :server "drchrono.irc.grove.io" :port 6697
+           :nick "nickmeharry" :full-name "Nick Meharry")
+  (setq erc-autojoin-channels-alist '(("#dev" "#drchrono" "#random"))))
