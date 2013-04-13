1d72acc41f90a59005db3875bdf6a9eb8e5812ba
Nick Meharry <nick@nickmeharry.com>
2013-04-13 16:28:47 -0700

    Markdown code face is rather unreadable in 23

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -294,4 +294,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-pre-face ((t (:inherit font-lock-constant-face :foreground "green")))))
