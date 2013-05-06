34f74db772cafc4ea599f817367dcdd47f74ced8
Nick Meharry <nick@drchrono.com>
2013-05-06 13:24:16 -0700

    Use OS X copy/paste

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -218,8 +218,17 @@
                        ((buffer-modified-p) "*  ")))
           "%b  (" invocation-name "@" system-name ")")))
 
-(defun copy-to-clipboard
-  (interactive))
+(when (eq system-type 'darwin)
+  (defun copy-from-osx ()
+    (shell-command-to-string "pbpaste"))
+  (setq interprogram-paste-function 'copy-from-osx)
+
+  (defun paste-to-osx (text &optional push)
+    (let* ((process-connection-type nil)
+           (proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc)))
+  (setq interprogram-cut-function 'paste-to-osx))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
