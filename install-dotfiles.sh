c3603822b363e3d1183d15aa9498eb21e09c9849
Nick Meharry <nick@drchrono.com>
2013-05-29 14:40:01 -0700

    Clean up installation script

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -38,12 +38,10 @@ for df in $dotfiles; do
     source="$dotfiles_dir/$df"
     target=".$df"
     if [ $target != "$(basename $target)" -a ! -e "$(dirname $target)" ]; then
-	echo "\033[33mCreating directories \"$(dirname $target)\"\033[0m"
-	mkdir -p "$(dirname $target)"
+        echo "Creating directories \"$(dirname $target)\""
+        mkdir -p "$(dirname $target)"
     fi
-    if [ -e $target ]; then
-        echo "\033[31mWarning! \"$target\" already exists!\033[0m"
-    else
+    if [ ! -e $target ]; then
         echo "Linking $source to $target"
         ln -s $source $target
     fi
