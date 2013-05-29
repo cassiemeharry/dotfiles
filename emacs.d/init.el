7f1b68ed9e17a629b5f2be288db0ebad6266c795
Nick Meharry <nick@drchrono.com>
2013-05-29 14:37:33 -0700

    Add a few more extensions for python-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -152,6 +152,10 @@ point reaches the beginning or end of the buffer, stop there."
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.htm$" . web-mode))
 
+; Extra Python extensions
+(add-to-list 'auto-mode-alist '("\\.wsgi$" . python-mode))
+(add-to-list 'auto-mode-alist '("\\.hl7t$" . python-mode))
+
 ; JS/Flymake
 (require 'flymake-jshint)
 (add-hook 'javascript-mode-hook
